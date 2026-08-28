10fd1ecd87f6b0c71dae612757d6978dff9474f8
Gao-jinn <2325880538@qq.com>
2026-08-28 19:16:11 +0800

    添加分号

--- src/main/java/com/xiyangyang/App.java
+++ src/main/java/com/xiyangyang/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("你在哪里")
+        System.out.println("你在哪里");
         System.out.println("我在这里呀");
         System.out.println("来抓我呀");
         System.out.println("Hello World!");
